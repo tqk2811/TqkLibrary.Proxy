dc27417e7995d138d0f1fc066be27223fb02fbc4
tqk2811 <tqk2811@gmail.com>
2022-12-19 18:10:54 +0700

    Socks4ProxySource

---
+++ TqkLibrary.Proxy/ProxySources/Socks4ProxySource.cs
@@ -0,0 +1,122 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using TqkLibrary.Proxy.Interfaces;
+using TqkLibrary.Proxy.StreamHeplers;
+
+namespace TqkLibrary.Proxy.ProxySources
+{
+    public class Socks4ProxySource : IProxySource, ISocks4Proxy
+    {
+        enum Socks4_CMD : byte
+        {
+            EstablishStreamConnection = 0x01,
+            EstablishPortBinding = 0x02,
+        }
+        enum Socks4_REP : byte
+        {
+            RequestGranted = 0x5a,
+            RequestRejectedOrFailed = 0x5b,
+            RequestFailedBecauseClientIsNotRunningIdentd = 0x5c,
+            RequestFailedBecauseClientIdentdCouldNotConfirmTheUserIdInTheRequest = 0x5d
+        }
+
+
+        const byte Socks4Version = 0x04;
+
+        readonly IPEndPoint iPEndPoint;
+        readonly string userId;
+        public Socks4ProxySource(IPEndPoint iPEndPoint, string userId = null)
+        {
+            this.iPEndPoint = iPEndPoint ?? throw new ArgumentNullException(nameof(iPEndPoint));
+            this.userId = userId ?? string.Empty;
+        }
+
+
+        public bool IsSupportUdp => false;
+        public bool IsSupportIpv6 => false;
+
+        public async Task<ISessionSource> InitSessionAsync(Uri address)
+        {
+            if (address == null) throw new NullReferenceException(nameof(address));
+
+            TcpClient tcpClient = new TcpClient();
+            Stream networkStream = null;
+            bool isSuccess = false;
+            try
+            {
+                IPAddress iPAddress = null;
+                switch (address.HostNameType)
+                {
+                    case UriHostNameType.IPv4:
+                        IPAddress.TryParse(address.Host, out iPAddress);
+                        break;
+                    case UriHostNameType.Dns:
+                        iPAddress = Dns.GetHostAddresses(address.Host).Where(x => x.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+                        break;
+
+                    default: throw new NotSupportedException(nameof(address.HostNameType));
+                }
+                if (iPAddress == null)
+                    throw new InvalidDataException();
+
+
+                await tcpClient.ConnectAsync(iPEndPoint.Address, iPEndPoint.Port);
+                networkStream = tcpClient.GetStream();
+
+                //================first request====================
+                UInt16 port = (UInt16)address.Port;
+                byte[] ipv4_bytes = iPAddress.GetAddressBytes();
+                byte[] buffer = new byte[]
+                {
+                    Socks4Version,
+                    (byte)Socks4_CMD.EstablishStreamConnection,
+                    (byte)(port >> 8),
+                    (byte)(port),
+                    ipv4_bytes[0],
+                    ipv4_bytes[1],
+                    ipv4_bytes[2],
+                    ipv4_bytes[3],
+                };
+                await networkStream.WriteAsync(buffer, 0, buffer.Length);
+
+                buffer = new byte[this.userId.Length + 1];
+                buffer[this.userId.Length] = 0;
+                if (!string.IsNullOrWhiteSpace(this.userId))
+                {
+                    Encoding.ASCII.GetBytes(this.userId, 0, this.userId.Length, buffer, 0);
+                }
+                await networkStream.WriteAsync(buffer, 0, buffer.Length);
+                await networkStream.FlushAsync();
+
+
+                //================first reply====================
+                buffer = await networkStream.ReadBytesAsync(8);
+                switch ((Socks4_REP)buffer[1])
+                {
+                    case Socks4_REP.RequestGranted:
+                        isSuccess = true;
+                        return new TcpStreamSessionSource(tcpClient);
+
+                    default:
+                        throw new Exception(((Socks4_REP)buffer[1]).ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[{nameof(Socks4ProxySource)}.{nameof(InitSessionAsync)}] {ex.GetType().FullName}: {ex.Message}, {ex.StackTrace}");
+            }
+            finally
+            {
+                if (!isSuccess)
+                {
+                    networkStream?.Dispose();
+                    networkStream = null;
+                    tcpClient?.Dispose();
+                    tcpClient = null;
+                }
+            }
+            return null;
+        }
+    }
+}

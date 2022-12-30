524adfbd57f9597c126df83562c175f7610fefb6
tqk2811 <tqk2811@gmail.com>
2022-12-30 17:37:58 +0700

    Socks4_REP: shorted name and add docs

--- TqkLibrary.Proxy/Enums/Socks4_REP.cs
+++ TqkLibrary.Proxy/Enums/Socks4_REP.cs
@@ -2,9 +2,21 @@
 {
     internal enum Socks4_REP : byte
     {
+        /// <summary>
+        /// Request granted
+        /// </summary>
         RequestGranted = 0x5a,
+        /// <summary>
+        /// Request rejected or failed
+        /// </summary>
         RequestRejectedOrFailed = 0x5b,
-        RequestFailedBecauseClientIsNotRunningIdentd = 0x5c,
-        RequestFailedBecauseClientIdentdCouldNotConfirmTheUserIdInTheRequest = 0x5d
+        /// <summary>
+        /// Request Failed Because Client Is Not Running Identd
+        /// </summary>
+        ClientIsNotRunningIdentd = 0x5c,
+        /// <summary>
+        /// Request Failed Because Client Identd Could Not Confirm The User Id In The Request
+        /// </summary>
+        CouldNotConfirmTheUserId = 0x5d
     }
 }

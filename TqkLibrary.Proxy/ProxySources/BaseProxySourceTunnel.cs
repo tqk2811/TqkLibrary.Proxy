716572dbc43255781252725fa92bd91ac41e5473
tqk2811 <tqk2811@gmail.com>
2023-02-03 15:17:45 +0700

    add BaseProxySourceTunnel

---
+++ TqkLibrary.Proxy/ProxySources/BaseProxySourceTunnel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TqkLibrary.Proxy.Interfaces;
+
+namespace TqkLibrary.Proxy.ProxySources
+{
+    internal abstract class BaseProxySourceTunnel<T> : IDisposable
+        where T : IProxySource
+    {
+        protected readonly T _proxySource;
+        protected readonly CancellationToken _cancellationToken;
+        protected readonly TcpClient _tcpClient = new TcpClient();
+        protected Stream _stream;
+        protected BaseProxySourceTunnel(T proxySource, CancellationToken cancellationToken = default)
+        {
+            this._proxySource = proxySource ?? throw new ArgumentNullException(nameof(proxySource));
+            this._cancellationToken = cancellationToken;
+        }
+        ~BaseProxySourceTunnel()
+        {
+            Dispose(false);
+        }
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        void Dispose(bool isDisposing)
+        {
+            _stream?.Dispose();
+            _tcpClient?.Dispose();
+        }
+    }
+}

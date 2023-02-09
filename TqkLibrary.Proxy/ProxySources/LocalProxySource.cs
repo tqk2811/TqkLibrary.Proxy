999630a782aac3290e8027f1fcd7a4660bc761f4
tqk2811 <tqk2811@gmail.com>
2023-02-09 16:24:24 +0700

    LocalProxySource add BindSourceTunnel

--- TqkLibrary.Proxy/ProxySources/LocalProxySource.cs
+++ TqkLibrary.Proxy/ProxySources/LocalProxySource.cs
@@ -15,11 +15,22 @@ using TqkLibrary.Proxy.StreamHeplers;
 
 namespace TqkLibrary.Proxy.ProxySources
 {
-    public class LocalProxySource : IProxySource, IHttpProxy
+    public partial class LocalProxySource : IProxySource, IHttpProxy
     {
+        public LocalProxySource()
+        {
+
+        }
+
+        public LocalProxySource(IPAddress bindIpAddress)
+        {
+            this.BindIpAddress = bindIpAddress;
+        }
+
         public bool IsSupportUdp { get; set; } = true;
         public bool IsSupportIpv6 { get; set; } = true;
         public bool IsSupportBind { get; set; } = true;
+        public IPAddress BindIpAddress { get; set; }
 
         public async Task<IConnectSource> InitConnectAsync(Uri address, CancellationToken cancellationToken = default)
         {
@@ -69,9 +80,48 @@ namespace TqkLibrary.Proxy.ProxySources
             }
         }
 
-        public Task<IBindSource> InitBindAsync(Uri address, CancellationToken cancellationToken = default)
+        async Task<IPAddress[]> GetLocalIpAddress()
         {
-            throw new NotImplementedException();
+            IPHostEntry iPHostEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            return iPHostEntry.AddressList;
+        }
+        static readonly IEnumerable<IPAddress> InvalidIPAddresss = new IPAddress[]
+        {
+            null,
+            IPAddress.Any,
+            IPAddress.Loopback,
+            IPAddress.Broadcast,
+            IPAddress.IPv6Any,
+            IPAddress.IPv6Loopback,
+        };
+        public async Task<IBindSource> InitBindAsync(Uri address, CancellationToken cancellationToken = default)
+        {
+            if (address is null) throw new ArgumentNullException(nameof(address));
+
+            //check if socks4, mustbe return ipv4. 
+            //on socks5, return ipv6 if have & need 
+            if (address.HostNameType != UriHostNameType.IPv4 || address.HostNameType != UriHostNameType.IPv6)
+                throw new InvalidDataException($"{nameof(address)} mustbe {nameof(UriHostNameType.IPv4)} or {nameof(UriHostNameType.IPv6)}");
+
+            IPAddress ipAddress = BindIpAddress;
+            if (InvalidIPAddresss.Any(x => x == ipAddress))
+            {
+                ipAddress = null;
+
+                //if invalid 
+                var addresses_s = await GetLocalIpAddress();
+
+                if (address.HostNameType == UriHostNameType.IPv6)
+                    ipAddress = addresses_s.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetworkV6);
+
+                if (ipAddress is null)
+                    ipAddress = addresses_s.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+
+                if (ipAddress is null)
+                    return null;
+            }
+
+            return new BindSourceTunnel(this, ipAddress);
         }
 
 

8e21ebef4bb98cd8e1c35e90f622d96d1f6712e6
tqk2811 <tqk2811@gmail.com>
2023-12-09 17:27:20 +0700

    add authentication

---
+++ TqkLibrary.Proxy/Authentications/BaseProxyAuthentication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Proxy.Authentications
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseProxyAuthentication
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override abstract bool Equals(object obj);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override abstract int GetHashCode();
+    }
+}
---
+++ TqkLibrary.Proxy/Authentications/HttpProxyAuthentication.cs
@@ -0,0 +1,34 @@
+﻿namespace TqkLibrary.Proxy.Authentications
+{
+    public class HttpProxyAuthentication : BaseProxyAuthentication
+    {
+        public HttpProxyAuthentication(string userName, string password)
+        {
+            if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentNullException(nameof(userName));
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password));
+            this.UserName = userName;
+            this.Password = password;
+        }
+
+        public string UserName { get; }
+        public string Password { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is HttpProxyAuthentication httpProxyAuthentication)
+            {
+                return this.GetHashCode() == httpProxyAuthentication.GetHashCode();
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return $"{UserName}|{Password}".GetHashCode();
+        }
+    }
+}

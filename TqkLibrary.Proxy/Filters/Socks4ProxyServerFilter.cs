f28d8aee7e93ed7dae1aaebd9098adb8902c7526
tqk2811 <tqk2811@gmail.com>
2023-12-13 15:37:32 +0700

    ProxyServerFilter add parent

--- TqkLibrary.Proxy/Filters/Socks4ProxyServerFilter.cs
+++ TqkLibrary.Proxy/Filters/Socks4ProxyServerFilter.cs
@@ -8,12 +8,21 @@ namespace TqkLibrary.Proxy.Filters
 {
     public class Socks4ProxyServerFilter : BaseProxyServerFilter
     {
+        const bool _IsSupportSock4A = false;
+        readonly Socks4ProxyServerFilter _parent;
+        public Socks4ProxyServerFilter()
+        {
 
+        }
+        public Socks4ProxyServerFilter(Socks4ProxyServerFilter parent) : base(parent)
+        {
+            this._parent = parent;
+        }
 
-        public bool IsUseSocks4A { get; set; } = true;
         public virtual Task<bool> IsUseSocks4AAsync(CancellationToken cancellationToken = default)
         {
-            return Task.FromResult(IsUseSocks4A);
+            if (_parent is not null) return _parent.IsUseSocks4AAsync();
+            else return Task.FromResult(_IsSupportSock4A);
         }
     }
 }

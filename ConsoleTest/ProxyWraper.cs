0b2e7ca3ef135ee394f445fde9e6f6474d8e5fbe
tqk2811 <tqk2811@gmail.com>
2023-12-20 17:46:15 +0700

    consoletest listen

--- ConsoleTest/ProxyWraper.cs
+++ ConsoleTest/ProxyWraper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using TqkLibrary.Proxy.Authentications;
@@ -16,21 +17,37 @@ namespace ConsoleTest
         const string listen = "127.0.0.1:16615";
         public static async Task RunAsync()
         {
-            using var server = new HttpProxyServer(IPEndPoint.Parse(listen), GetProxySource());
+            string strHostName = Dns.GetHostName();
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(strHostName);
+            IPAddress? ipaddress = null;
+
+            ipaddress = iPHostEntry
+                    .AddressList
+                    .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+
+            IPEndPoint ipEndPoint = IPEndPoint.Parse(listen);
+            if (ipaddress is not null)
+            {
+                ipEndPoint = new IPEndPoint(ipaddress, 0);
+            }
+
+            ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            using var server = new HttpProxyServer(ipEndPoint, GetProxySource());
             server.StartListen();
-            Console.WriteLine($"Listening {listen}");
+            Console.WriteLine($"Listening {server.IPEndPoint}");
             Console.ReadLine();
         }
 
         static IProxySource GetProxySource()
         {
             HttpProxyAuthentication? auth = null;// new HttpProxyAuthentication("", "");
-            HttpProxySource httpProxySource = new HttpProxySource(new Uri("http://167.99.142.56:20128"));
+            HttpProxySource httpProxySource = new HttpProxySource(new Uri("http://88.99.245.58:8903"));
             Socks4ProxySource socks4ProxySource = new Socks4ProxySource(IPEndPoint.Parse("93.104.63.65:80"));
             Socks5ProxySource socks5ProxySource = new Socks5ProxySource(IPEndPoint.Parse("138.201.120.118:29127"));
             LocalProxySource localProxySource = new LocalProxySource();
 
-            return localProxySource;
+            return httpProxySource;
         }
     }
 }

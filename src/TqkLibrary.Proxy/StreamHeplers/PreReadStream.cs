e94cddc385f5ae586a8f3e34822976ea18b41d8f
tqk2811 <tqk2811@gmail.com>
2025-03-04 18:02:18 +0700

    fix PreReadStream

--- src/TqkLibrary.Proxy/StreamHeplers/PreReadStream.cs
+++ src/TqkLibrary.Proxy/StreamHeplers/PreReadStream.cs
@@ -53,7 +53,31 @@ namespace TqkLibrary.Proxy.StreamHeplers
             while (true);
         }
 
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int length = _ReadPreBuffer(buffer, offset, count);
+            if (length > 0)
+            {
+                return length;
+            }
+            else
+            {
+                return _baseStream.Read(buffer, offset, count);
+            }
+        }
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            int length = _ReadPreBuffer(buffer, offset, count);
+            if (length > 0)
+            {
+                return length;
+            }
+            else
+            {
+                return await _baseStream.ReadAsync(buffer, offset, count, cancellationToken);
+            }
+        }
+        int _ReadPreBuffer(byte[] buffer, int offset, int count)
         {
             if (_preReadBuffer is not null)
             {
@@ -73,153 +97,11 @@ namespace TqkLibrary.Proxy.StreamHeplers
                     return lengthCopy;
                 }
             }
-            else
-            {
-                return await _baseStream.ReadAsync(buffer, offset, count);
-            }
+            return -1;
         }
-
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            return _baseStream.WriteAsync(buffer, offset, count, cancellationToken);
-        }
-
-
-
-
-
-
-
 
         public override bool CanSeek => false;
         public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
         public override void SetLength(long value) => throw new NotSupportedException();
-
-
-
-
-        public override int Read(byte[] buffer, int offset, int count)
-            => throw new NotSupportedException();
-        //{
-        //    if (_preReadBuffer is not null)
-        //    {
-        //        int canReadSize = Math.Min(buffer.Length - offset, count);
-        //        if (canReadSize >= _preReadBuffer.Length)
-        //        {
-        //            int lengthCopy = _preReadBuffer.Length;
-        //            Array.Copy(_preReadBuffer, 0, buffer, offset, lengthCopy);
-        //            _preReadBuffer = null;
-        //            return lengthCopy;
-        //        }
-        //        else
-        //        {
-        //            int lengthCopy = canReadSize;
-        //            Array.Copy(_preReadBuffer, 0, buffer, offset, lengthCopy);
-        //            _preReadBuffer = _preReadBuffer.Skip(lengthCopy).ToArray();
-        //            return lengthCopy;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return _baseStream.Read(buffer, offset, count);
-        //    }
-        //}
-        public override void Write(byte[] buffer, int offset, int count)
-            => throw new NotSupportedException();
-        //{
-        //    _baseStream.Write(buffer, offset, count);
-        //}
-
-        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
-            => throw new NotSupportedException();
-
-        public override void EndWrite(IAsyncResult asyncResult)
-            => throw new NotSupportedException();
-
-
-        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
-            => throw new NotSupportedException();
-        //{
-        //    if (_preReadBuffer is not null)
-        //    {
-        //        return new AsyncResult(this, buffer, offset, count, callback, state).TryCallback();
-        //    }
-        //    else
-        //    {
-        //        return _baseStream.BeginRead(buffer, offset, count, callback, state);
-        //    }
-        //}
-        public override int EndRead(IAsyncResult asyncResult)
-            => throw new NotSupportedException();
-        //{
-        //    if (asyncResult is AsyncResult myAsyncResult)
-        //    {
-        //        return myAsyncResult.ReadByte(this);
-        //    }
-        //    else
-        //    {
-        //        return _baseStream.EndRead(asyncResult);
-        //    }
-        //}
-
-
-        //class AsyncResult : IAsyncResult
-        //{
-        //    readonly PreReadStream _preReadStream;
-        //    readonly byte[] _buffer;
-        //    readonly int _offset;
-        //    readonly int _count;
-        //    readonly AsyncCallback? _callback;
-        //    readonly object? _state;
-        //    readonly ManualResetEvent manualResetEvent = new ManualResetEvent(false);
-        //    public AsyncResult(PreReadStream preReadStream, byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
-        //    {
-        //        _preReadStream = preReadStream;
-        //        _buffer = buffer;
-        //        _offset = offset;
-        //        _count = count;
-        //        _callback = callback;
-        //        _state = state;
-        //        manualResetEvent.Set();
-        //    }
-
-        //    public object? AsyncState => _state;
-        //    public WaitHandle AsyncWaitHandle => manualResetEvent;
-        //    public bool CompletedSynchronously => true;
-        //    public bool IsCompleted => true;
-
-
-        //    public IAsyncResult TryCallback()
-        //    {
-        //        if (_callback is not null)
-        //            Task.Run(() => _callback.Invoke(this));
-        //        return this;
-        //    }
-
-        //    public int ReadByte(PreReadStream preReadStream)
-        //    {
-        //        if (preReadStream._preReadBuffer is null || preReadStream._preReadBuffer.Length == 0)
-        //        {
-        //            preReadStream._preReadBuffer = null;
-        //            return 0;
-        //        }
-
-        //        int canReadSize = Math.Min(_buffer.Length - _offset, _count);
-        //        if (canReadSize >= preReadStream._preReadBuffer.Length)
-        //        {
-        //            int lengthCopy = preReadStream._preReadBuffer.Length;
-        //            Array.Copy(preReadStream._preReadBuffer, 0, _buffer, _offset, lengthCopy);
-        //            preReadStream._preReadBuffer = null;
-        //            return lengthCopy;
-        //        }
-        //        else
-        //        {
-        //            int lengthCopy = canReadSize;
-        //            Array.Copy(preReadStream._preReadBuffer, 0, _buffer, _offset, lengthCopy);
-        //            preReadStream._preReadBuffer = preReadStream._preReadBuffer.Skip(lengthCopy).ToArray();
-        //            return lengthCopy;
-        //        }
-        //    }
-        //}
     }
 }
